45477030fe813649e46f2fdcf01b5aded7d97c84
ailyntang <ailyntang@gmail.com>
2019-10-22 16:58:41 +1100

    [P-002] Add category model

--- Source/Models/Category.swift
+++ Source/Models/Category.swift
@@ -1,9 +1,33 @@
-//
-//  File.swift
-//  productive
-//
-//  Created by Ai-Lyn Tang on 22/10/19.
-//  Copyright © 2019 CoderLyn. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+enum Category {
+  case health
+  case fitness
+  case home
+  case hobbies
+  case social
+  case efficiency
+  
+  var title: String {
+    switch self {
+    case .health: return "Health"
+    case .fitness: return "Fitness"
+    case .home: return "Home"
+    case .hobbies: return "Hobbies"
+    case .social: return "Social"
+    case .efficiency: return "Efficiency"
+    }
+  }
+  
+  var icon: UIImage? {
+    switch self {
+    case .health: return #imageLiteral(resourceName: "iconPharmacy")
+    case .fitness: return #imageLiteral(resourceName: "iconDumbbell")
+    case .home: return #imageLiteral(resourceName: "iconWashingMachine")
+    case .hobbies: return #imageLiteral(resourceName: "iconStar")
+    case .social: return #imageLiteral(resourceName: "iconWomanMan")
+    case .efficiency: return #imageLiteral(resourceName: "iconClock")
+    }
+  }
+}
