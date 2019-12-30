d06cdaf2b1178e15ef5e5cf3b985fea0cb952798
Ai-Lyn Tang <ailyntang@gmail.com>
2019-12-30 14:17:19 +1100

    Add placeholder table view and collection view for Create Habit

---
+++ Source/Features/CreateHabit/CreateHabitCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CreateHabitCollectionViewCell.swift
+//  productive
+//
+//  Created by Ai-Lyn Tang on 30/12/19.
+//  Copyright © 2019 CoderLyn. All rights reserved.
+//
+
+import UIKit
+
+class CreateHabitCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Source/Features/CreateHabit/CreateHabitViewController.swift
+++ Source/Features/CreateHabit/CreateHabitViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 final class CreateHabitViewController: UIViewController {
-  private var viewModel: CreateHabitViewModel!
+    @IBOutlet private var tableView: UITableView! {
+        didSet {
+            tableView.register(cellType: IconTitleCell.self)
+        }
+    }
+    
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.register(CreateHabitCollectionViewCell.self, forCellWithReuseIdentifier: Text.cellIdentifier)
+        }
+    }
+    
+    private var viewModel: CreateHabitViewModel!
 
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
@@ -24,4 +36,36 @@ final class CreateHabitViewController: UIViewController {
     super.viewDidLoad()
     title = viewModel.navBarTitle
   }
+}
+
+extension CreateHabitViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeue(cellType: IconTitleCell.self, for: indexPath)
+        
+        let imageView = UIImageView(image: UIImage(named: "iconPencil")?.withRenderingMode(.alwaysTemplate))
+        imageView.tintColor = .gray
+        
+        cell.configure(with: IconTitleCellDisplayModel(icon: imageView.image!, title: "Name of habit"))
+        cell.textLabel?.textColor = .gray
+        return cell
+    }
+}
+
+extension CreateHabitViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Text.cellIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+private enum Text {
+    static let cellIdentifier = "CollectionViewCell"
 }
